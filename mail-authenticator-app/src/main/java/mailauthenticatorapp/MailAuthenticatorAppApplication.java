cd290da9694ec44b6e91b6a81ac961bbf3e8e07b
Radhika shahade <radhikashahade0804@gmail.com>
2023-12-10 14:26:48 +0530

    new project

---
+++ mail-authenticator-app/src/main/java/mailauthenticatorapp/MailAuthenticatorAppApplication.java
@@ -0,0 +1,13 @@
+package mailauthenticatorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailAuthenticatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailAuthenticatorAppApplication.class, args);
+	}
+
+}
