b92b72c150f1fdd957b42a7250ad5b2f40693169
Radhika shahade <radhikashahade0804@gmail.com>
2023-12-13 10:08:09 +0530

    new project

--- mail-authenticator-app/src/main/java/mailauthenticatorapp/repository/UserRepository.java
+++ mail-authenticator-app/src/main/java/mailauthenticatorapp/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package mailauthenticatorapp.repository;
 
-import mailauthenticatorapp.entity.AppUser;
+import mailauthenticatorapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<AppUser, Integer> {
-    Optional<AppUser> findByEmail(String email);
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
 }
