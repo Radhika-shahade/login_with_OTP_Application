a2304c796328e16a242ce5a59d4e092d0f2dd55e
Radhika shahade <radhikashahade0804@gmail.com>
2023-12-13 10:07:35 +0530

    new project

---
+++ mail-authenticator-app/src/main/java/mailauthenticatorapp/dto/UserResponse.java
@@ -0,0 +1,14 @@
+package mailauthenticatorapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse {
+    private String message;
+}
